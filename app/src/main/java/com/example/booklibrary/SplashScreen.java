610fbec3ad455adae3b5511d39d6aebc45139a07
evitadop <evitaputrii14@gmail.com>
2021-12-22 23:59:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/booklibrary/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.booklibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
